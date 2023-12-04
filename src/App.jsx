05e6fe554b2dc3563ce0733cb754ec8ba85ed4bc
rohanprajapati <rohanprajapati369@gmail.com>
2023-12-04 22:30:11 +0545

    created a react business landing page

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./component/Navbar/Navbar";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Navbar/> 
+
+      <Navbar/> 
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <>
+      <div className="container">
+        <div id="menu">
+          <div id="logo">Pay</div>
+          <div className="menu-item">HOME</div>
+          <div className="menu-item">FEATURES</div>
+          <div className="menu-item">DETAILS</div>
+          <div className="menu-item">PRICING</div>
+          <div className="menu-item">VIDEO</div>
+          <div className="menu-item round-btn">LOGIN</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
